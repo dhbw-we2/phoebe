bd8a6768d0fefe545615d3e9f3a386b8a5c484ab
Manuel Piroch <manuel@pepit.net>
2020-11-23 21:15:44 +0100

    cleanup

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="fixed-center text-center">
-    <p>
-      <img
-        src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
-      >
-    </p>
+    <q-icon name="eva-alert-circle-outline" size="50px" class="q-ma-lg"/>
     <p class="text-faded">
       Sorry, nothing here...<strong>(404)</strong>
     </p>
     <q-btn
-      color="secondary"
+      color="primary"
       style="width:200px;"
       to="/"
-      label="Go back"
+      label="Home"
     />
   </div>
 </template>
 
-<script >
+<script>
 export default {
   name: 'Error404'
 }
