8fa963d613652b44d51ab66c6918ba4481aa8deb
Manuel Piroch <manuel@pepit.net>
2020-12-10 23:19:25 +0100

    updated PostView skeleton

--- src/components/forum/posts/PostSkeleton.vue
+++ src/components/forum/posts/PostSkeleton.vue
@@ -7,17 +7,27 @@
         <q-btn flat round icon="eva-arrow-ios-downward-outline" disable/>
       </q-card-actions>
       <div class="full-width">
-        <q-card-section vertical class="q-pl-none q-pb-none">
-          <q-item class="q-pl-none q-pr-none q-pb-md">
+        <q-card-section vertical class="q-pa-none q-pt-md">
+          <q-item class="q-pl-none">
             <q-item-section avatar>
               <q-skeleton type="QAvatar" size="60px"/>
             </q-item-section>
             <q-item-section>
-              <q-skeleton type="text"/>
-              <q-skeleton type="text" width="80%"/>
+              <q-skeleton type="text" width="40%"/>
+              <q-skeleton type="text" width="60%"/>
             </q-item-section>
           </q-item>
-          <q-skeleton height="110px" class="q-mb-sm"/>
+          <q-card-section :horizontal="!$q.screen.xs && !$q.screen.sm" class="q-pa-none">
+            <q-card-section class="col-6 q-pl-none q-pb-none">
+              <spotify-item-display :spotify-item="null"/>
+            </q-card-section>
+            <q-card-section class="q-pb-none full-width">
+              <q-skeleton type="text" class="text-h5 q-pb-md" width="60%"/>
+              <q-skeleton type="text"/>
+              <q-skeleton type="text"/>
+              <q-skeleton type="text"/>
+            </q-card-section>
+          </q-card-section>
         </q-card-section>
         <q-card-actions class="full-width">
           <q-space/>
@@ -30,8 +40,11 @@
 </template>
 
 <script>
+import SpotifyItemDisplay from "components/forum/SpotifyItemDisplay";
+
 export default {
   name: "PostSkeleton",
+  components: {SpotifyItemDisplay},
 }
 </script>
 
