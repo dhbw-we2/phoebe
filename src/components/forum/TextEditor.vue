9185d08d5729fe521de48472eaaf6f3c1b67ca33
Manuel Piroch <manuel@pepit.net>
2020-12-12 01:19:29 +0100

    improved emoji menu

--- src/components/forum/TextEditor.vue
+++ src/components/forum/TextEditor.vue
@@ -1,44 +1,42 @@
 <template>
   <div>
-      <q-editor class=""
-                ref="editor"
-                v-model="textInputProxy"
-                :placeholder=placeholderText
-                :dense="$q.screen.lt.md"
-                @paste.native="evt => pasteCapture(evt)"
-                @drop.prevent
-                content-class="links-primary"
-                :toolbar="
+    <q-editor class=""
+              ref="editor"
+              v-model="textInputProxy"
+              :placeholder=placeholderText
+              :dense="$q.screen.lt.md"
+              @paste.native="evt => pasteCapture(evt)"
+              @drop.prevent
+              content-class="links-primary"
+              :toolbar="
                 [
                   ['bold', 'italic', 'strike', 'underline'],
                   ['unordered', 'ordered'],
                   ['undo', 'redo'],
                   ['emoji']
                 ]"
-      >
-        <template v-slot:emoji>
-          <q-btn
-            dense no-caps
-            ref="token"
-            no-wrap
-            unelevated
-            size="sm"
-            icon="eva-smiling-face-outline"
-            @click="toggleEmojiDialog"
-          />
-        </template>
-      </q-editor>
-    <div v-if="emojiDialog" ref="emojiDialogRef"
-         class="absolute-bottom-right"
-         style="outline: none"
-         tabindex="-1"
-         @focusout="emojiDialog = false">
-      <VEmojiPicker
-        @select="selectEmoji"
-        dark="true"
-        class="emoji-picker-color-override"
-         />
-    </div>
+    >
+      <template v-slot:emoji>
+        <q-btn
+          dense no-caps
+          ref="token"
+          no-wrap
+          unelevated
+          size="sm"
+          icon="eva-smiling-face-outline">
+          <q-menu v-model="emojiDialog" cover anchor="bottom left"
+                  transition-show="jump-down"
+                  transition-hide="jump-up"
+                  content-class="bg-transparent">
+            <VEmojiPicker
+              @select="selectEmoji"
+              dark="true"
+              class="emoji-picker-color-override"
+            />
+          </q-menu>
+        </q-btn>
+      </template>
+    </q-editor>
   </div>
 </template>
 
@@ -70,27 +68,16 @@ export default {
     }
   },
   methods: {
-    toggleEmojiDialog() {
-      this.emojiDialog = !this.emojiDialog
-      if (this.emojiDialog) {
-        this.$nextTick(function () {
-          this.$refs.emojiDialogRef.focus();
-        });
-      }
-    },
     selectEmoji(emoji) {
-      this.$nextTick(function () {
-        this.$refs.emojiDialogRef.focus();
-        this.emojiDialog = true;
-      });
       this.$refs.editor.runCmd('insertText', emoji.data)
+      this.emojiDialog = true
     },
     pasteCapture(evt) {
       let text, onPasteStripFormattingIEPaste
       evt.preventDefault()
       if (evt.originalEvent && evt.originalEvent.clipboardData.getData) {
         text = evt.originalEvent.clipboardData.getData('text/plain')
-        if(text.startsWith("http")){
+        if (text.startsWith("http")) {
           const linkHTML = `<a href="${text}" target="_blank" rel="noopener noreferrer">${text}</a>`;
           this.$refs.editor.runCmd('insertHTML', linkHTML)
         } else {
@@ -98,7 +85,7 @@ export default {
         }
       } else if (evt.clipboardData && evt.clipboardData.getData) {
         text = evt.clipboardData.getData('text/plain')
-        if(text.startsWith("http")){
+        if (text.startsWith("http")) {
           const linkHTML = `<a href="${text}" target="_blank" rel="noopener noreferrer">${text}</a>`;
           this.$refs.editor.runCmd('insertHTML', linkHTML)
         } else {
